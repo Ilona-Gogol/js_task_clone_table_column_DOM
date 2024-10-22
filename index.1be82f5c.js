8cb2d5310834f53fa36bb5194136b6cc1d2bbe36
Ilona Gogol <ilonka.gogol@gmail.com>
2024-10-22 15:29:23 +0300

    make build

---
+++ index.1be82f5c.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("tr").forEach(function(e){if(e.children.length>1){var n=e.children[1].cloneNode(!0);e.insertBefore(n,e.children[e.children.length-1])}});
+//# sourceMappingURL=index.1be82f5c.js.map
