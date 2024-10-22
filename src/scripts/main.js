986e871172f4f0a44d7d8a329d676cfa9faf3ffb
Ilona Gogol <ilonka.gogol@gmail.com>
2024-10-22 15:28:04 +0300

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,14 @@ const trs = document.querySelectorAll('tr');
 
 trs.forEach((el) => {
   const parentElement = el;
-  const firstChild = parentElement.children[1];
-  const clonedChild = firstChild.cloneNode(true);
 
-  parentElement.insertBefore(clonedChild, parentElement.children[4]);
+  if (parentElement.children.length > 1) {
+    const firstChild = parentElement.children[1];
+    const clonedChild = firstChild.cloneNode(true);
+
+    parentElement.insertBefore(
+      clonedChild,
+      parentElement.children[parentElement.children.length - 1],
+    );
+  }
 });
